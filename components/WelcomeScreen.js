f319f0337913bd97e4af262b945b0b20c88aa73a
nevinsebastian <heynevinhere@gmail.com>
2024-05-25 23:05:22 +0530

    first commit

---
+++ components/WelcomeScreen.js
@@ -0,0 +1,15 @@
+// WelcomeScreen.js
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const WelcomeScreen = ({ navigation }) => {
+ 
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome Screen</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
