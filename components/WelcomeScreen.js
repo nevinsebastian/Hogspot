fd3299656d80ad8162c4594c1975aeef9402f765
nevinsebastian <heynevinhere@gmail.com>
2024-05-26 11:12:26 +0530

    image

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,15 +1,34 @@
 // WelcomeScreen.js
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
- 
-
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Welcome Screen</Text>
+    <View style={styles.container}>
+      <Image 
+        source={require('../assets/Group.png')} 
+        style={styles.image} 
+      />
+      <Text style={styles.text}>Welcome Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff', // Optional: to set a background color
+  },
+  image: {
+    width: 311, // Adjust the width of the image
+    height: 306, // Adjust the height of the image
+    marginBottom: 20, // Space between image and text
+  },
+  text: {
+    fontSize: 24, // Adjust the size of the text
+  },
+});
 
 export default WelcomeScreen;
