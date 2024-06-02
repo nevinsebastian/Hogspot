dc891276314c35dba4d0fbffd383140e7c2d5443
nevinsebastian <heynevinhere@gmail.com>
2024-06-02 19:25:23 +0530

    hi

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,32 +1,49 @@
 
 
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { SvgXml } from 'react-native-svg';
+import LinearGradient from 'react-native-linear-gradient';
 
 const bellSvg = `
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_201_658)">
-<path d="M10 5C10 4.46957 10.2107 3.96086 10.5858 3.58579C10.9609 3.21071 11.4696 3 12 3C12.5304 3 13.0391 3.21071 13.4142 3.58579C13.7893 3.96086 14 4.46957 14 5C15.1484 5.54303 16.1274 6.38833 16.8321 7.4453C17.5367 8.50227 17.9404 9.73107 18 11V14C18.0753 14.6217 18.2954 15.2171 18.6428 15.7381C18.9902 16.2592 19.4551 16.6914 20 17H4C4.54494 16.6914 5.00981 16.2592 5.35719 15.7381C5.70457 15.2171 5.92474 14.6217 6 14V11C6.05956 9.73107 6.4633 8.50227 7.16795 7.4453C7.8726 6.38833 8.85159 5.54303 10 5" stroke="#4B164C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M9 17V18C9 18.7956 9.31607 19.5587 9.87868 20.1213C10.4413 20.6839 11.2044 21 12 21C12.7956 21 13.5587 20.6839 14.1213 20.1213C14.6839 19.5587 15 18.7956 15 18V17" stroke="#4B164C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<circle cx="18" cy="6" r="3.25" fill="#DD88CF" stroke="white" stroke-width="1.5"/>
-</g>
-<defs>
-<clipPath id="clip0_201_658">
-<rect width="24" height="24" fill="white"/>
-</clipPath>
-</defs>
+<path d="M12 2C13.1 2 14 2.9 14 4H10C10 2.9 10.9 2 12 2ZM18 8V11C18 12.66 18.79 14.14 20 15.1V16H4V15.1C5.21 14.14 6 12.66 6 11V8C6 5.79 7.79 4 10 4H14C16.21 4 18 5.79 18 8ZM16 17H8V18H16V17Z" fill="#4B164C"/>
 </svg>
-
 `;
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Hogspot</Text>
-      <TouchableOpacity style={styles.notificationButton}>
-        <SvgXml xml={bellSvg} style={styles.bellIcon} />
-      </TouchableOpacity>
+      <View style={styles.header}>
+        <Text style={styles.heading}>Hogspot</Text>
+        <TouchableOpacity style={styles.notificationButton}>
+          <SvgXml xml={bellSvg} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.rectangleContainer}>
+        <LinearGradient
+          colors={['#7D4E76', '#E38DD5']}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.gradientBorder}
+        >
+          <View style={styles.rectangleContent}>
+            <View style={styles.circleContainer}>
+              <Image
+                source={require('../assets/lulu.jpg')}
+                style={styles.circleImage}
+              />
+            </View>
+            <Text style={styles.rectangleText}>Lulu, Kochi</Text>
+          </View>
+        </LinearGradient>
+        <View style={styles.externalShape}>
+          <Image
+            source={require('../assets/profile.jpg')}
+            style={styles.externalImage}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -34,11 +51,15 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    paddingHorizontal: 16,
     backgroundColor: '#FFFFFF',
+  },
+  header: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    paddingTop: 58,
+    paddingBottom: 10,
   },
   heading: {
     fontSize: 28,
@@ -52,17 +73,73 @@ const styles = StyleSheet.create({
     width: 48,
     height: 48,
     borderRadius: 24,
-    backgroundColor: '#F5F5F5',
+    borderWidth: 1,
+    borderColor: '#4B164C',
     justifyContent: 'center',
     alignItems: 'center',
     position: 'absolute',
     right: 16,
     top: 52,
   },
-  bellIcon: {
-    width: 24,
-    height: 24,
-    color: '#4B164C' ,
+  rectangleContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  gradientBorder: {
+    width: 271,
+    height: 70,
+    borderRadius: 32,
+    padding: 2,
+  },
+  rectangleContent: {
+    flex: 1,
+    backgroundColor: '#F8E7F6',
+    borderRadius: 28,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  circleContainer: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    borderWidth: 4,
+    borderColor: '#DD88CF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 8,
+  },
+  circleImage: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+  },
+  rectangleText: {
+    fontSize: 24,
+    fontFamily: 'Inter-Bold',
+    color: '#4B164C',
+    marginLeft: 16,
+  },
+  externalShape: {
+    position: 'absolute',
+    right: -35, // Adjust to position correctly
+    top: -2,
+    width: 70,
+    height: 67,
+    borderRadius: 16,
+    borderWidth: 4,
+    borderColor: '#DD88CF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  externalImage: {
+    width: 62,
+    height: 59,
+    borderRadius: 16,
   },
 });
 
