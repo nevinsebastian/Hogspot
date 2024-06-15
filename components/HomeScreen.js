2eafc381ca07c58c58411c12c70cd232d47b4a6a
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-06-15 17:00:46 +0530

    profile and hogspot heading

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -29,7 +29,7 @@ const HomeScreen = () => {
         <View style={styles.innerRectangle}>
           <Image source={require('../assets/lulu.jpg')} style={styles.image} />
         </View>
-        <Text style={styles.locationText}>Lulu, kochi</Text>
+        <Text style={styles.locationText}>Lulu, Kochi</Text>
       </View>
 
       <View style={styles.newRectangle}>
