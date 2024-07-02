9600fe2a477e55aa1a791748ac350fbe1985920c
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-07-02 10:56:59 +0530

    added icons in navbar

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -41,8 +41,17 @@ const discoverSvg = `
 
 const plusSvg = `
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12 5V19M5 12H19" stroke="#4B164C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<g clip-path="url(#clip0_1347_317)">
+<path d="M12 5V19" stroke="#4B164C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5 12H19" stroke="#4B164C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_1347_317">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
 </svg>
+
 `;
 
 const matchesSvg = `
