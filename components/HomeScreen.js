7f012f257ee7ef12fddbf4c6fb508264779ef1d0
nevinsebastian <heynevinhere@gmail.com>
2025-01-25 23:11:24 +0530

    streek

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -6,6 +6,8 @@ import SwipeCards from 'react-native-swipe-cards';
 import { useNavigation } from '@react-navigation/native';
 
 
+
+
 const bellSvg = `
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_201_658)">
